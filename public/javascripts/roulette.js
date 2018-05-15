6fdaa6e62a0167e18bb9f78b9f38b67f9a90287b
qshurick <aleksandr.malyk@werkspot.nl>
2018-05-15 13:50:30 +0200

    fix mobile safari compatibility
    safari doesn't allow to use same name for variables declared as `const`
    or `let` and elements ids

--- public/javascripts/roulette.js
+++ public/javascripts/roulette.js
@@ -1,4 +1,4 @@
-const projects = [];
+const projectStorage = [];
 
 const bulletPosition = [
   'translate(15vh, 6vh)',
@@ -23,18 +23,18 @@ function addProject(name, projectList, bullets) {
     return false;
   }
 
-  if (projects.indexOf(name) !== -1) {
+  if (projectStorage.indexOf(name) !== -1) {
     alert(`Project with name ${name} already in the list, choose another one`);
     return false;
   }
 
   const item = document.createElement('li');
 
-  projects.push(name);
+  projectStorage.push(name);
   item.innerText = name;
   projectList.appendChild(item);
-  bullets[projects.length - 1].innerHTML = name[0].toLocaleUpperCase();
-  bullets[projects.length - 1].style.transform = bulletPosition[projects.length - 1];
+  bullets[projectStorage.length - 1].innerHTML = name[0].toLocaleUpperCase();
+  bullets[projectStorage.length - 1].style.transform = bulletPosition[projectStorage.length - 1];
 
   document.getElementById('load-sound').play();
 
@@ -43,7 +43,7 @@ function addProject(name, projectList, bullets) {
 
 (function () {
   const projectName = document.getElementById('project-name');
-  const projectList = document.getElementById('projects');
+  const projectList = document.getElementById('project');
   const roulette = document.getElementsByClassName('revolver')[0];
   const bullets = document.getElementsByClassName('bullet');
 
@@ -54,7 +54,7 @@ function addProject(name, projectList, bullets) {
 
     projectName.value = '';
 
-    if (projects.length >= maxBulletAmount) {
+    if (projectStorage.length >= maxBulletAmount) {
       this.disabled = true;
       projectName.disabled = true;
     } else {
@@ -67,14 +67,14 @@ function addProject(name, projectList, bullets) {
     projectName.disabled = true;
     this.disabled = true;
 
-    if (projects.length < 6) {
-      for (let projectsIndex = projects.length; projectsIndex < 6; projectsIndex++) {
+    if (projectStorage.length < 6) {
+      for (let projectsIndex = projectStorage.length; projectsIndex < 6; projectsIndex++) {
         addProject(`empty-${projectsIndex}`, projectList, bullets);
       }
     }
 
-    const list = document.querySelectorAll('#projects li');
-    const projectIndex = Math.floor(Math.random() * projects.length);
+    const list = document.querySelectorAll('#projectStorage li');
+    const projectIndex = Math.floor(Math.random() * projectStorage.length);
 
     const stopAngle = spins * 360 + bulletHolderAngle * projectIndex;
     let angle = 0;
@@ -85,12 +85,12 @@ function addProject(name, projectList, bullets) {
         bullets[bulletIndex].style.transform = `${bulletPosition[bulletIndex]} rotate(${-angle}deg)`;
       }
 
-      const index = Math.round((angle % 360) / bulletHolderAngle) % projects.length;
+      const index = Math.round((angle % 360) / bulletHolderAngle) % projectStorage.length;
 
       if (index >= 1) {
         list[index - 1].style.background = '';
       } else {
-        list[projects.length - 1].style.background = '';
+        list[projectStorage.length - 1].style.background = '';
       }
 
       list[index].style.background = itemHighlightColor;
